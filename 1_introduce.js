f6110cd67f8d89b236a4f01cb0b8c5e223727a6e
412ock <412ock.dev@gmail.com>
2023-03-30 00:39:12 +0900

    Chapter 1 Start

--- 1_introduce.js
+++ 1_introduce.js
@@ -1,7 +1,20 @@
-function addMaker(a){
-    return function(b){
-        return a+b;
-    }
-}
+const {expect} = require("chai");
 
-addMaker(10)(5);
+describe("Chapter 1", ()=>{
+    function addMaker(a){
+        return function(b){
+            return a+b;
+        }
+    }
+
+    it("Result addMaker", ()=>{
+        expect(addMaker(10)(5)).to.equal(15);
+    })
+
+    it("Result addMaker using 5", ()=>{
+        const add5 = addMaker(5);
+        expect(add5(3)).to.equal(8);
+        expect(add5(4)).to.equal(9);
+    })
+})
+
