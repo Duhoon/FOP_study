45c038d5f4b0e3e012ced37d9ddb78471e748122
Duhoon <eclip6@naver.com>
2023-04-18 14:37:21 +0900

    0416_study

--- 3_1_underscore.js
+++ 3_1_underscore.js
@@ -183,5 +183,190 @@ _.filter = function(data, predicate){
     return result;
 }
 
+console.log( "# Filter" );
 console.log( _.filter([1,2,3,4,], v=> v > 1) );
 console.log( _.filter({a:1, b:2, c:3, d:4}, v => v > 1) );
+
+/************************************/
+/*       Filter with new Bloop      */
+/************************************/
+
+function bloop(new_data, body){
+    return function(data, iter_predi){
+        const result = new_data();
+        if( isArrayLike(data) ){
+            for( let i = 0, len = data.length; i < len; i++ ){
+                body ( iter_predi(data[i], i, data), result, data[i] );
+            }
+        } else{
+            for (let i = 0, keys=_.keys(data), len = keys.length; i < len ; i++ ){
+                body( iter_predi(data[keys[i]], keys[i], data), result, data[keys[i]] );
+            }
+        }
+        return result;
+    }
+}
+
+_.filter = bloop(_.array, 
+    function(bool, result, val){
+        if(bool) result.push(val);
+});
+
+console.log("\n # Improved Filter")
+console.log( _.filter([1,2,3,4,], v=> v > 1) );
+console.log( _.filter({a:1, b:2, c:3, d:4}, v => v > 1) );
+
+/************************************/
+/*               Rest               */
+/************************************/
+
+_.toArray = function(list){
+    return Array.isArray(list) ? list : _.values(list);
+}
+
+_.rest = function(list, num){
+    return _.toArray(list).slice(num || 1);
+};
+
+console.log("\n # Rest");
+console.log( _.rest([1, 2, 3, 4], 2) );
+
+_.reverse = function(list){
+    return _.toArray(list).reverse();
+};
+
+console.log( _.reverse([1, 2, 3, 4]) );
+console.log( _.reverse({0: 2000, 1:3000, 2: 10000}) )
+
+/************************************/
+/*              Rester              */
+/************************************/
+
+// _.rester = function(func){
+//     return function(list, num){
+//         return _.map( _.rest(list,num), func);
+//     }
+// }
+
+_.rester = function(func, num){
+    return function(){
+        return func.apply(null, _.rest(arguments, num));
+    };
+}
+
+function sum(a, b, c, d){
+    return ( (a || 0) + (b || 0) + (c || 0) + (d || 0) );
+}
+
+console.log("\n # Rester");
+console.log( _.rester(sum, 2)(1, 2, 3, 4) );
+
+/************************************/
+/*                If                */
+/************************************/
+_.if = function(validator, func, alter){
+    return function(){
+        return validator.apply(null, arguments) ? 
+        func.apply(null, arguments) :
+        alter && alter.apply(null, arguments);
+    }
+};
+
+function sub(a, b){
+    return a - b;
+}
+
+var sub2 = _.if(
+    function(a, b){
+        return a > b;
+    },
+    sub,
+    function(){
+        return new Error( "a가 b보다 작습니다" );
+    }
+)
+
+var diff = _.if(
+    function(a, b){
+        return a > b;
+    },
+    sub,
+    function(a, b){return sub(b,a)}
+);
+
+console.log( "\n # If" )
+console.log( sub2(10,5) );
+console.log( sub2(5,10) );
+
+console.log( diff(5, 10));
+
+_.toArray2 = _.if(Array.isArray, _.idtt, _.values);
+
+_.constant = function(v){
+    return function(){
+        return v;
+    }
+}
+
+var square = _.if(
+    function(a){ return toString.call(a) === "[object Number]"; },
+    function(a){ return a * a; },
+    function(){return 0}
+)
+
+console.log( square(5) );
+console.log( square({}) );
+
+_.isNumber = function(a){
+    return toString.call(a) === "[object Number]";
+}
+
+var square = _.if(_.isNumber, v => v * v, ()=>0);
+console.log( square(5) );
+
+
+/*************************************/
+/* Filter without Anonymous Function */
+/*************************************/
+
+_.push = function(obj, val){
+    obj.push(val);
+    return obj;
+}
+_.filter = bloop(_.array, _.if(_.idtt, _.rester(_.push)));
+
+console.log("# filter");
+console.log(
+    _.filter([1,2,3,4], function(val){
+        return val > 2;
+    })
+)
+
+function bloop(new_data, body){
+    return function(data, iter_predi){
+        let result = new_data(data);
+        if( isArrayLike(data) ){
+            for(let i = 0, len = data.length; i < len ; i++){
+                body( iter_predi(data[i], i, data), result, data[i]);
+            }
+        } else {
+            for(let i = 0, keys = _.keys(data), len = keys.length ; len < i ; i++){
+                body( iter_predi(data[keys[i]], keys[i], data), result, data[keys[i]] );
+            }
+        }
+        return result;
+    }
+}
+
+/************************************/
+/*              Reject              */
+/************************************/
+
+_.reject = bloop(_.array, _.if( _.idtt, _.noop, _.rester(_.push) ))
+
+console.log("# Reject")
+console.log(
+    _.reject([1,2,3,4], function(val){
+        return val > 2;
+    })
+)
